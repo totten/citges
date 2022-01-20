406f95a5ec2b3b63ae595d858dc165bd9342e401
Tim Otten <totten@civicrm.org>
2022-01-19 17:43:13 -0800

    dummy-jsonrpc.php - Better mock data

--- scripts/dummy-jsonrpc.php
+++ scripts/dummy-jsonrpc.php
@@ -17,6 +17,14 @@ function send($data) {
   echo json_encode($data) . "\n";
 }
 
+function respond($request, $result) {
+  send([
+    'jsonrpc' => '2.0',
+    'result' => $result,
+    'id' => $request['id'] ?? NULL,
+  ]);
+}
+
 $PROG = basename(__FILE__);
 $maxRequests = $argv[1] ?? NULL;
 
@@ -37,11 +45,22 @@ $i = 0;
 while ($line = fgets(STDIN)) {
   $i++;
   $request = json_decode($line, TRUE);
-  send([
-    'jsonrpc' => '2.0',
-    'result' => ['msg' => 'processed request ' . $i],
-    'id' => $request['id'] ?? NULL,
-  ]);
+
+  $sig = $request['method'];
+  if ($request['method'] === 'api3' || $request['method'] === 'api4') {
+    $sig .= ':' . $request['params'][0] . '.' . $request['params'][1];
+  }
+
+  switch ($sig) {
+    case 'api4:Queue.get':
+      respond($request, []);
+      break;
+
+    default:
+      respond($request, ['msg' => 'processed request ' . $i]);
+      break;
+  }
+
   if (is_numeric($maxRequests) && $i >= $maxRequests) {
     break;
   }
